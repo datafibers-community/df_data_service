7160498fb2d8ae2e86fc952e11e16841edf9fb51
Will <willddy@gmail.com>
2017-02-24 15:43:40 -0500

    added poc for dynamic code generation

--- src/main/java/com/datafibers/test_tool/CodeGenFlinkTable.java
+++ src/main/java/com/datafibers/test_tool/CodeGenFlinkTable.java
@@ -1,12 +1,26 @@
 package com.datafibers.test_tool;
-import com.datafibers.test_tool.FlinkUDF;
 import net.openhft.compiler.CompilerUtils;
+import org.apache.flink.api.java.table.StreamTableEnvironment;
+import org.apache.flink.api.table.Table;
+import org.apache.flink.api.table.TableEnvironment;
 
 /**
  * The goal is to define a function with parameter TABLE, Transformation Script and return TABLE
  * This function will dynamically generate code and run.
  */
-public class CodeGen {
+public class CodeGenFlinkTable {
+
+	public static Table getFlinkTableObj(String className, String javaCode){
+		try {
+			Class aClass = CompilerUtils.CACHED_COMPILER.loadFromJava(className, javaCode);
+			DynamicRunner runner = (DynamicRunner) aClass.newInstance();
+			//return runner.getTableObj();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	public static void main(String args[]) {
 		String udfPackage = "com.datafibers.test_tool.FlinkUDF.";
@@ -20,10 +34,12 @@ public class CodeGen {
 				"import org.apache.flink.api.java.ExecutionEnvironment;\n" +
 				"import org.apache.flink.api.common.functions.FlatMapFunction;\n" +
 				"import org.apache.flink.api.java.tuple.Tuple2;\n" +
+				"import com.datafibers.test_tool.*;\n" +
 				"import org.apache.flink.util.Collector;";
 
 		String javaCode = header +
-				"public class WordCount implements Runnable {\n" +
+				"public class WordCount implements DynamicRunner {\n" +
+				"@Override \n" +
 				"    public void run() {\n" +
 				"       final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();\n" +
 				"		DataSet<String> text = env.fromElements(" + article + ");\n" +
@@ -37,7 +53,7 @@ public class CodeGen {
 
 		try {
 			Class aClass = CompilerUtils.CACHED_COMPILER.loadFromJava(className, javaCode);
-			Runnable runner = (Runnable) aClass.newInstance();
+			DynamicRunner runner = (DynamicRunner) aClass.newInstance();
 			runner.run();
 
 		} catch (Exception e) {
---
+++ src/main/java/com/datafibers/test_tool/DynamicRunner.java
@@ -0,0 +1,11 @@
+package com.datafibers.test_tool;
+
+import org.apache.flink.api.table.Table;
+
+/**
+ * Created by DUW3 on 2/24/2017.
+ */
+public interface DynamicRunner {
+    void run();
+    //Table getTableObj();
+}
