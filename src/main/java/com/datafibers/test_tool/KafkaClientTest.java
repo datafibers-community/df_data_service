4de30db156ff7f6128b441ada8da656ce0b7d8cc
dadu <willddy@gmail.com>
2017-09-08 15:36:30 -0400

    added kafka admin client

--- src/main/java/com/datafibers/test_tool/KafkaClientTest.java
+++ src/main/java/com/datafibers/test_tool/KafkaClientTest.java
@@ -1,40 +1,12 @@
 package com.datafibers.test_tool;
 
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.common.errors.TimeoutException;
-import org.apache.kafka.common.errors.TopicExistsException;
-
-import java.util.Collections;
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
-
+import com.datafibers.util.KafkaAdminClient;
 
 public class KafkaClientTest {
 
     public static void main(String[] args) {
-        System.out.println("TestCase_New Kafka Admin Client");
-        Properties props = new Properties();
-        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        AdminClient adminClient = AdminClient.create(props);
-        NewTopic topic = new NewTopic("my_topic", 2, (short)1);
-        CreateTopicsResult createTopicsResult = adminClient.createTopics(Collections.singleton(topic));
-
-        try {
-            createTopicsResult.all().get();
-            // real failure cause is wrapped inside the raised ExecutionException
-        } catch (ExecutionException | InterruptedException e) {
-            if (e.getCause() instanceof TopicExistsException) {
-                System.err.println("Topic already exists !!");
-            } else if (e.getCause() instanceof TimeoutException) {
-                System.err.println("Timeout !!");
-            }
-            e.printStackTrace();
-        } finally {
-            adminClient.close();
-        }
-
+        KafkaAdminClient.listTopics().forEach(System.out::println);
+        //new KafkaAdminClient().deleteTopics("my_topic");
+        //System.out.println(new KafkaAdminClient().existsTopic("df_meta"));
     }
 }
