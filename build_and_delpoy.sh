4ee37438b0ed0287db31848aa0ab5b826ab8b423
DataFibers Admin <datafibers@gmail.com>
2017-09-15 17:09:47 -0400

    Update build_and_delpoy.sh

--- build_and_delpoy.sh
+++ build_and_delpoy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 npm run build
+echo "Deploy the UI code to data service ..."
 rm -r -f ../df_data_service/src/main/resources/dfa/
 cp -r build/* ../df_data_service/src/main/resources/dfa/
+echo "Deployed. Do not forget to checkin the changes in df_data_service"
