b3c7b9292164615c5283e30c91ff28e2e997c211
DataFibers Admin <datafibers@gmail.com>
2017-09-15 17:00:56 -0400

    Update build_and_delpoy.sh

--- build_and_delpoy.sh
+++ build_and_delpoy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 npm run build
-rm -rf ../df_data_service/src/main/resources/apidoc/
-cp -r ./build/* ../df_data_service/src/main/resources/apidoc/
+rm -r -f ../df_data_service/src/main/resources/dfa/
+cp -r build/* ../df_data_service/src/main/resources/dfa/
