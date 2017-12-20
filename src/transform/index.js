4e46de0d34e7bf9bf68f409ef5cb9c1b2ad4ba7a
dadu <willddy@gmail.com>
2017-12-20 08:35:27 -0500

    update spark sql ui

--- src/transform/index.js
+++ src/transform/index.js
@@ -90,17 +90,8 @@ export const TransformEdit = (props) => (
 		        </DependentInput>
 		        <DependentInput dependsOn="connectorType" value="TRANSFORM_EXCHANGE_SPARK_SQL">
                     <BooleanInput source="connectorConfig.stream_back_flag" label="Stream Back the Result?" defaultValue={false} style={{ width: 500 }} />
-                    <DependentInput dependsOn="connectorConfig.stream_back_flag" value={true}>
-                        <BooleanInput source="connectorConfig.choose_or_create" label="New or Choose Topic?" defaultValue={false} style={{ width: 500 }} />
-                        <DependentInput dependsOn="connectorConfig.choose_or_create" value={true}>
-                            <LongTextInput source="connectorConfig.stream_back_topic" label="Auto Create a Write Topic" style={{ width: 500 }}/>
-                        </DependentInput>
-                        <DependentInput dependsOn="connectorConfig.choose_or_create" value={false}>
-                            <ReferenceInput source="connectorConfig.stream_back_topic" label="Choose a Write Topic Existed" reference="schema" validate={[ required ]} allowEmpty>
-                                <AutocompleteInput optionText="subject" />
-                            </ReferenceInput>
-                        </DependentInput>
-                    </DependentInput>
+                    <BooleanInput source="connectorConfig.choose_or_create" label="New or Choose Topic?" defaultValue={false} style={{ width: 500 }} />
+                    <LongTextInput source="connectorConfig.stream_back_topic" label="Stream Back Topic" style={{ width: 500 }}/>
 		            <LongTextInput source="connectorConfig.trans_sql" label="Spark SQL over Hive Queries, such as select * from ..." validate={[ required ]} style={{ width: 500 }} />
 		        </DependentInput>
 		        <DependentInput dependsOn="connectorType" value="TRANSFORM_EXCHANGE_FLINK_Script">
