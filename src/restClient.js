d696ffe0ada8b1e1303e0612be972f506c98e70e
Will <willddy@gmail.com>
2019-07-21 11:30:56 -0400

    modify ip in package.json

--- src/restClient.js
+++ src/restClient.js
@@ -1,6 +1,6 @@
 import { jsonServerRestClient } from 'admin-on-rest';
 
 // const restClient = simpleRestClient('http://localhost:3000');
-const restClient = jsonServerRestClient('http://localhost:8080/api/df');
+const restClient = jsonServerRestClient('http://3.13.64.3:8080/api/df');
 // export restClient
 export default (type, resource, params) => new Promise(resolve => setTimeout(() => resolve(restClient(type, resource, params)), 500));
